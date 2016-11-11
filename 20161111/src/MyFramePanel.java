7dce67d5635791cbc1e198e956cdef7b0249ec91
nsaori <sappy1pappince@gmail.com>
2016-11-11 15:03:28 +0900

    Jpanel 추가

---
+++ 20161111/src/MyFramePanel.java
@@ -0,0 +1,28 @@
+//20161111 saori
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFramePanel extends JFrame {
+
+	public MyFramePanel() {
+		this.setTitle("panel");
+		this.setSize(300, 250);
+		
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(3, 2, 30, 25));
+		
+		p.add(new Button("1"));
+		p.add(new Button("2"));
+		p.add(new Button("3"));
+		p.add(new Button("4"));
+		p.add(new Button("5"));
+		p.add(new Button("6"));
+		
+		this.add(p);
+		
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
