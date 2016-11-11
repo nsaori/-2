20ca225bc3f920fdd3e5e772161513c519bc4430
nsaori <sappy1pappince@gmail.com>
2016-11-11 14:18:21 +0900

    borderlayout

--- 20161111/src/MyFrameTest.java
+++ 20161111/src/MyFrameTest.java
@@ -4,8 +4,9 @@ public class MyFrameTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyFrame mft = new MyFrame();
-
+		//MyFrame mf = new MyFrame();
+		MyFrameBoderLayout fbl = new MyFrameBoderLayout();
+		
 	}
 
 }
