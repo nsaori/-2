a88d431ba547f4c0aa7f9b3e6153456c6ae0b970
nsaori <sappy1pappince@gmail.com>
2016-11-11 14:49:30 +0900

    grid layout 추가

---
+++ 20161111/src/MyFrameGridLayout.java
@@ -0,0 +1,26 @@
+//20161111 saori
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrameGridLayout extends JFrame {
+
+	public MyFrameGridLayout(){
+		
+		this.setTitle("gridLayout");
+		this.setSize(300, 250);
+		
+		Container cp = this.getContentPane();
+		cp.setLayout(new GridLayout(3, 2,30,20));
+		
+		cp.add(new JButton("1"));
+		cp.add(new JButton("2"));
+		cp.add(new JButton("3"));
+		cp.add(new JButton("4"));
+		cp.add(new JButton("5"));
+		//cp.add(new JButton("6"));
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
