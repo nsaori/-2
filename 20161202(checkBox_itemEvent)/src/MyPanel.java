87232180d4caf2c51d81e35a35692dff1bf1d653
nsaori <sappy1pappince@gmail.com>
2016-12-02 15:19:11 +0900

    check box 미완성

--- 20161202(checkBox_itemEvent)/src/MyPanel.java
+++ 20161202(checkBox_itemEvent)/src/MyPanel.java
@@ -1,6 +1,9 @@
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.*;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel implements ItemListener{
 
 	public JLabel ml1,ml2;
 	public JCheckBox[] cb = new JCheckBox[3];
@@ -19,5 +22,23 @@ public class MyPanel extends JPanel {
 		
 		ml2 = new JLabel("");
 		this.add(ml2);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		JCheckBox source = (JCheckBox)e.getSource();
+		
+		int count = 0;
+		if(cb[0].isSelected()){
+			count += 100;
+		}
+		if(cb[1].isSelected()){
+			count += 500;
+		}
+		if(cb[2].isSelected()){
+			count+=20000;
+			
+		}
+		ml2.setText(count + "¿ø");
 	}
 }
