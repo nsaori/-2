f43f0c63cd2d041635039d5e1fbb09cc4158b2a4
nsaori <sappy1pappince@gmail.com>
2016-12-02 14:40:43 +0900

    actionlistener 등록할 수 이ㅏㅆ는 component 주의

--- 20161202(GUI)/src/MyListener.java
+++ 20161202(GUI)/src/MyListener.java
@@ -9,30 +9,32 @@ import javax.swing.*;
 public class MyListener implements ActionListener {
 
 	private MyPanel p;
-	
+
 	public MyListener(MyPanel p) {
 		this.p = p;
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
-		JButton b = (JButton)arg0.getSource();
-		
-		if(b == p.b1 ){	//b1 선택
+
+		JButton b = (JButton) arg0.getSource();
+
+		if (b == p.b1) { // b1 선택
 			System.out.println("버튼의 위치와 크기");
-			System.out.println("위치 = (" + b.getX() + " , " + b.getY() + " )" );
-			System.out.println("크기 = (" + b.getWidth() + " , " + b.getHeight() + " )" );
-			System.out.println("크기 = " + b.getSize() + "\n");
-			
-		}else if(b == p.b2){	//b2 선택
+			System.out.println("위치 = (" + b.getX() + " , " + b.getY() + " )");
+			//System.out.println("위치 = " + b.getLocation());
+			System.out.println("크기 = (" + b.getWidth() + " , " + b.getHeight() + " )\n");
+			//System.out.println("크기 = " + b.getSize() + "\n");
+
+		} else if (b == p.b2) { // b2 선택
 			System.out.println(b.getFont() + "\n");
-			
-		}else{	//b3 선택; hide,show
-			//if(p.b1.isVisible() == true)
+
+		} else { // b3 선택; hide,show
+			// if(p.b1.isVisible() == true)
 			p.b1.setVisible(!p.b1.isVisible());
 			p.b2.setVisible(!p.b2.isVisible());
 			p.b3.setVisible(!p.b3.isVisible());
-			
+
 		}
 	}
 
--- 20161202(GUI)/src/MyPanel.java
+++ 20161202(GUI)/src/MyPanel.java
@@ -10,24 +10,23 @@ public class MyPanel extends JPanel {
 	public JButton b4;
 	
 	public MyPanel(){
-		MyListener l = new MyListener(this);
 		
 		b1 = new JButton("위치와 크기 정보");
-		b1.setSize(60, 20);
-		b1.setLocation(51, 1);
-		b1.addActionListener(l);
-		
+				
 		b2 = new JButton("모양정보");
 		b2.setBackground(Color.yellow);
 		b2.setForeground(Color.red);
 		b2.setFont(new Font("이텔릭체", Font.ITALIC, 20));
-		b2.addActionListener(l);
 		
 		b3 = new JButton("작동하지 않은 버튼");
 		b3.setEnabled(false);
 		
 		b4 = new JButton("숨기기/보이기");
-		b4.addActionListener(l);
+		
+		MyListener ml = new MyListener(this);
+		b1.addActionListener(ml);
+		b2.addActionListener(ml);
+		b4.addActionListener(ml);
 		
 		this.add(b1);
 		this.add(b2);
