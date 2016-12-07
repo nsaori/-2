08af280c5ef5f42fd765de7cb43f59173ccbc6e3
nsaori <sappy1pappince@gmail.com>
2016-12-07 17:20:03 +0900

    radio box - itemEvent

---
+++ 20161207(radioBox_itemEvent)/src/MyFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	public MyFrame(){
+		this.setTitle("radioBox item Event");
+		this.setSize(300, 250);
+		
+		this.add(new MyPanel());
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	
+}
---
+++ 20161207(radioBox_itemEvent)/src/MyPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MyPanel extends JPanel {
+	public JLabel ml1,ml2;
+	public JRadioButton[] rb = new JRadioButton[3];
+	public MyPanel() {
+		
+	}
+}
