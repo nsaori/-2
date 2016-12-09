3b6fb5c61630af287146f1509b9d96bd467840db
nsaori <sappy1pappince@gmail.com>
2016-12-09 15:06:19 +0900

    ActionListener 추가

--- 20161209(GUI-text...)/src/MyPanel.java
+++ 20161209(GUI-text...)/src/MyPanel.java
@@ -1,29 +1,52 @@
+
+//lactionListener lapper
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-
 public class MyPanel extends JPanel {
-	public JLabel ml1,ml2,ml3,ml4;
-	public JTextField tf1,tf2,tf3;
-	
-	public MyPanel(){
+	public JLabel ml1, ml2, ml3, ml4;
+	public JTextField tf1, tf2, tf3;
+
+	public MyPanel() {
 		ml1 = new JLabel("이름: ");
 		tf1 = new JTextField(8);
 		ml2 = new JLabel("학과: ");
 		tf2 = new JTextField("컴퓨터공학과 ");
-		tf2.setEnabled(false); //글 바꾸지 못하게!
+		tf2.setEnabled(false); // 글 바꾸지 못하게!
 		ml3 = new JLabel("주소: ");
-		tf3 = new JTextField("서울시...",15);
-		
-		//tf1.setSize(10, 30);
+		tf3 = new JTextField("서울시...", 15);
+
+		ml4 = new JLabel("입력결과: ");
+
+		// tf1.setSize(10, 30);
 		this.add(ml1);
 		this.add(tf1);
 		this.add(ml2);
 		this.add(tf2);
 		this.add(ml3);
 		this.add(tf3);
-		
-		ml4 = new JLabel();
 		this.add(ml4);
-		
+
+		MyListener ml = new MyListener();
+		tf1.addActionListener(ml);
+		tf2.addActionListener(ml);
+		tf3.addActionListener(ml);
+
+	}
+
+	// 글 쓰고 enter치면 label에 나오게 한다.
+	public class MyListener implements ActionListener {
+
+		String str = "";
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JTextField tf = (JTextField) e.getSource();
+			str += " , " + tf.getText();
+			ml4.setText(str);
+		}
+
 	}
 }
