484504ea9b71055dbe1f887460bd54ac24e8b4e1
nsaori <sappy1pappince@gmail.com>
2016-12-13 13:43:00 +0900

    Jlist Jtextfield

---
+++ 20161213(Jlist,JconboBOx,Jsiidos)/src/MyFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	public MyFrame(){
+		this.setTitle("");
+		this.setSize(300, 250);
+		
+		this.add(new MyPanel());
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
