0ed20766dff4074afbdbec40d599bded47544d78
nsaori <sappy1pappince@gmail.com>
2016-12-13 13:49:19 +0900

    jlist 추가

--- 20161213(Jlist,JconboBOx,Jsiidos)/src/MyPanel.java
+++ 20161213(Jlist,JconboBOx,Jsiidos)/src/MyPanel.java
@@ -1,5 +1,18 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class MyPanel extends JPanel {
+	public JList list;
+	public JTextField txtField;
+
+	public MyPanel() {
+		String[] str = {"컴공과","글로벌소프트웨어학과","수상생명공학과","경찰행정학과"}; 
+		
+		list = new JList(str);
+		this.add(list);
+		
+		txtField = new JTextField(10);
+		this.add(txtField);
+
+	}
 
 }
