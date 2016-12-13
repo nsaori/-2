f955c2da764d7a0358a8bd1e4c295f74149438f0
nsaori <sappy1pappince@gmail.com>
2016-12-13 13:52:17 +0900

    JScrollPane 추가

--- 20161213(Jlist,JconboBOx,Jsiidos)/src/MyPanel.java
+++ 20161213(Jlist,JconboBOx,Jsiidos)/src/MyPanel.java
@@ -5,11 +5,12 @@ public class MyPanel extends JPanel {
 	public JTextField txtField;
 
 	public MyPanel() {
-		String[] str = {"컴공과","글로벌소프트웨어학과","수상생명공학과","경찰행정학과"}; 
-		
+		String[] str = { "컴공과", "글로벌소프트웨어학과", "수상생명공학과", "경찰행정학과" };
+
 		list = new JList(str);
-		this.add(list);
-		
+		JScrollPane scrolpane = new JScrollPane(list);
+		this.add(scrolpane);
+
 		txtField = new JTextField(10);
 		this.add(txtField);
 
